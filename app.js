18a836de9ece46ebf525ccb2544b546cff2dd030
Ramsés López <ramses.lopezs@gmail.com>
2016-09-11 20:56:46 -0400

    preparacion de proyecto para uploader

--- app.js
+++ app.js
@@ -1,31 +1,91 @@
-var fs = require('fs')
-var _ = require('lodash')
-var inspect = require('util').inspect
-var _log = (t) => console.log(inspect(t, {colors: true, depth: Infinity}))
+/*
+ * Import required packages.
+ * Packages should be installed with "npm install".
+ */
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var aws = require('aws-sdk');
+var ejs = require('ejs');
 
-var tbc =
-	fs.readFileSync('data/teams_by_category.json', 'utf8')
+/*
+ * Set-up the Express app.
+ */
+var app = express();
+app.set('views', __dirname + '/views');
+app.engine('html', ejs.renderFile);
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
 
-tbc = JSON.parse(tbc)
+/*
+ * Load the S3 information from the environment variables.
+ */
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
-r = _.chain(tbc.data)
-    .groupBy("team.name")
-    .toPairs()
-    // .map(function(currentItem) {
-    //     delete currentItem.team
-    //     return currentItem
-    //     // return _.object(_.zip(["color", "users"], currentItem));
-    // })
-    .value();
+console.log(AWS_ACCESS_KEY,AWS_SECRET_KEY,S3_BUCKET)
+
+/*
+ * Respond to GET requests to /account.
+ * Upon request, render the 'account.html' web page in views/ directory.
+ */
+app.get('/', function(req, res){
+    res.render('web/index.html');
+});
+
+/*
+ * Respond to GET requests to /sign_s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and the
+ * anticipated URL of the image.
+ */
+app.get('/sign_s3', function(req, res){
+
+    console.log('get sign s3')
+
+    aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.file_name,
+        Expires: 60,
+        ContentType: req.query.file_type,
+        ACL: 'public-read'
+    };
+
+    s3.getSignedUrl('putObject', s3_params, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var return_data = {
+                signed_request: data,
+                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
+});
 
 
-// var r = _.groupBy(tbc.data, function(t){
-// 	//el group by pone como key del objeto lo retornado de esta funcion
+/*
+ * Respond to POST requests to /submit_form.
+ * This function needs to be completed to handle the information in
+ * a way that suits your application.
+ */
+app.post('/submit_form', function(req, res){
+    username = req.body.username;
+    full_name = req.body.full_name;
+    avatar_url = req.body.avatar_url;
+    update_account(username, full_name, avatar_url); // TODO: create this function
+    // TODO: Return something useful or redirect
+});
 
-// 	// return 'a'
-// 	return t.team_id
-// })
+/*
+ * Start the server to handle incoming requests.
+ */
+app.listen(app.get('port'), () => {
+	console.log('running on port ' + app.get('port'))
+});
 
-_log(r)
-
-process.exit(0)
--- server.js
+++ server.js
@@ -1,17 +1,55 @@
 var express = require('express');
 var app = express();
 
+var http = require('http');
+var path = require('path');
+var aws = require('aws-sdk');
+var ejs = require('ejs');
+
 app.use('/web', express.static(__dirname + '/web'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/lang', express.static(__dirname + '/lang'));
 
-app.all('/*', function(req, res, next) {
+app.get('/', function(req, res) {
     // Just send the index.html for other files to support HTML5Mode
     res.sendFile('web/index.html', { root: __dirname });
 });
 
 port = process.env.PORT || 3001;
 
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
+
 app.listen(port, function () {
 	console.log('Server started > http://locahost:' + port);
+	console.log('S3_BUCKET', S3_BUCKET)
+	console.log('AWS_ACCESS_KEY',AWS_ACCESS_KEY)
+	console.log('AWS_SECRET_KEY', AWS_SECRET_KEY)
+});
+
+app.get('/sign_s3', function(req, res){
+    aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.file_name,
+        Expires: 60,
+        ContentType: req.query.file_type,
+        ACL: 'public-read'
+    };
+
+    s3.getSignedUrl('putObject', s3_params, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var return_data = {
+                signed_request: data,
+                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
 });
